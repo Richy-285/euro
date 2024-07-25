b37124421b21aec53c3ba8eced6ca058f897c196
unknown <euro285richy@gmail.com>
2024-07-25 21:11:47 +0700

    my first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// Start 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
+
+// ตัวอย่างในบทช่วยสอนนี้สร้างขึ้นโดยใช้
+// แอปสร้างปฏิกิริยา
+
+// ติดตั้งแอป create-react-
+// และคุณจะสามารถเรียกใช้ตัวอย่างเดียวกันนี้บนคอมพิวเตอร์ของคุณได้
+
+// ในตัวอย่างนี้ เราสร้างส่วนประกอบที่เรียกว่า 'Hello'
+// ส่วนประกอบจะแสดงผลในคอนเทนเนอร์ที่เรียกว่า 'root'
+
+// End 
+
+
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+import { library, icon } from '@fortawesome/fontawesome-svg-core';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+
+library.add(...all)
+
+const camera = icon({ prefix: 'fas', iconName: 'camera' })
+
+var camera = window.FontAwesome.icon({ prefix: 'fas', iconName: 'camera' });
+
+
+import { faSword } from '@awesome.me/kit-KIT_CODE/classic/solid'
+import { faAxe } from '@awesome.me/kit-KIT_CODE/classic/regular'
+import { faBowArrow } from '@awesome.me/kit-KIT_CODE/classic/light'
+import { faHelmetBattle } from '@awesome.me/kit-KIT_CODE/classic/thin'
+import { faShieldCross } from '@awesome.me/kit-KIT_CODE/duotone/solid'
+import { faFaceHeadBandage } from '@awesome.me/kit-KIT_CODE/sharp/solid'
+import { faPawClaws } from '@awesome.me/kit-KIT_CODE/sharp/regular'
+import { faEyeEvil } from '@awesome.me/kit-KIT_CODE/sharp/light'
+import { faRing } from '@awesome.me/kit-KIT_CODE/sharp/thin'
+
+// Import uploaded icons from your Kit
+import { faMyIcon } from '@awesome.me/kit-KIT_CODE/icons/kit/custom'
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+
+
+
+
+import Head from "next/head";
+import Container from "react-bootstrap/Container";
+import AppGuides from "@/components/AppGuides";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ExampleComponents from "@/components/ExampleComponents";
+
+export default function Home() {
+  return ('');
+}
